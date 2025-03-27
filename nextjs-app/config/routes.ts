657f92830d2f4ecac67b19af40ef534b674573c0
Hana Pospíšilová <h.hedbavna@gmail.com>
2025-03-27 15:34:01 +0100

    feat: :wrench: define main-nav routes

---
+++ nextjs-app/config/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  home: { href: "/", label: "Úvod" },
+  news: { href: "/aktuality", label: "Aktuality" },
+  seeds: { href: "/seminka", label: "Semínka" },
+  getSeeds: { href: "/chci-seminka", label: "Jak získat semínka" },
+  production: { href: "/semenareni", label: "Semenaření" },
+  we: { href: "/o-nas", label: "O nás" },
+} as const;
